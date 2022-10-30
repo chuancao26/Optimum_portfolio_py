85545701f3ca5b768e662ffb9ca5fa689607318f
unknown <chuancao@unsa.edu.pe>
2022-10-30 11:23:39 -0500

    30 october

--- using_functions_portfolio.py
+++ using_functions_portfolio.py
@@ -1,6 +1,6 @@
 # Apartado de librerias necesarias para el procesamiento de datos 
 import os 
-os.chdir("E:/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py")
+os.chdir("/media/cristiandavid/CURSOS FEC/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py/")
 import classes 
 
 import numpy as np
@@ -15,97 +15,131 @@ from scipy.stats import skew, kurtosis, chi2
 importlib.reload(classes)
 # Ahora importo los scripts necesarios, como classes y functions para cargar los 
 # datos
+#%% Corrigiendo el cargador de datos 
+
+os.chdir("/media/cristiandavid/CURSOS FEC/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py/acciones/")
+asset = pd.read_csv('BBVA.MC.csv', usecols = ['Date'])
+bench = pd.read_csv("^STOXX.csv", usecols = ['Date'])
+asset = pd.to_datetime(asset.Date, dayfirst = True)
+bench = pd.to_datetime(bench.Date, dayfirst = True)
+asset['returns']
+
+time = set(asset) & set(bench)
+
+asset = asset[asset.isin(time)
+asset.reset_index()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #%% Funciones 
+direccion = '/media/cristiandavid/CURSOS FEC/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py/acciones/'
+   
 
-direccion = 'E:/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py/acciones/'
 
-def date(ric):
-    path = direccion + ric
-    date_df = pd.read_csv(path)
-    date_df = date_df.dropna() 
-    return len(date_df['Date'])
 
-def load_time_series(ric):
-    # con su extension csv   
-    # haciendo movil la entrada de datos
-    path = direccion
-    table_raw = pd.read_csv(path + ric)
-    # ahora convertiremos estos dates simples en datetimes
-    # para lo q creare un df
-    t = pd.DataFrame()
-    t['Date'] = pd.to_datetime(table_raw['Date'], dayfirst = True)
-    t['close'] = table_raw['Close']
-    # Hallando los retornos 
-    t['close_previous'] = table_raw.Close.shift(1)
-    t['return_close'] = t.close/t.close_previous - 1
-    t = t.dropna()      
-    t = t.reset_index(drop = True)
-    # reciclano el codigo para las metricas estadisticas
-    # Eliminando los na
-    x = t.return_close.values
-    x_str = 'Real Return of ' + ric
-    # x_size = len(x)
-    return x, x_str, t
-
-def time_series_graph_of(x, y, name = ''):
-    plt.figure()
-    plt.plot(x, y, color = 'r')
-    plt.title('Time Series Graph of ' + name)
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.show()
+# def base_retornos(rics):
+#     '''
+#         Formato de leido CSV. 
+#         Vector con el nombre de los archivos.csv
     
-def distributions_graph_of(x, ric, string_plot, bins = 100):
-    plt.figure()
-    plt.hist(x, bins)
-    plt.title('Grafico de distribucion para la variable ' + ric)
-    plt.xlabel(string_plot)
-    plt.tight_layout()
-    plt.show()
-
-def elementos(ric):
-    data = pd.read_csv(direccion + ric).dropna(axis = 0)
-    return data.shape[0]
-
-def base_retornos(rics):
-    '''
-        Formato de leido CSV. 
-        Vector con el nombre de los archivos.csv
-    
-        input: Vector de RICS 
-        return: Price df 
-                Return df
+#         input: Vector de RICS 
+#         return: Price df 
+#                 Return df
         
-    '''
-    # Primero hallamos el menor 
-    menor = 100000
-    for ric in rics:
-        if (elementos(ric) < menor):
-            menor = elementos(ric)
-            ric_menor = ric
+#     '''
+#     # Primero hallamos el menor 
+#     menor = 100000
+#     for ric in rics:
+#         if (elementos(ric) < menor):
+#             menor = elementos(ric)
+#             ric_menor = ric
     
-    base_retornos = pd.DataFrame()
-    base_prices = pd.DataFrame()
-    menor_date = pd.read_csv(direccion + ric_menor)
-    menor_date['Date'] = pd.to_datetime(menor_date.Date, dayfirst = True)
-    base_retornos['Date'] = menor_date.Date
-    base_prices['Date'] = menor_date.Date
+#     base_retornos = pd.DataFrame()
+#     base_prices = pd.DataFrame()
+#     menor_date = pd.read_csv(direccion + ric_menor)
+#     menor_date['Date'] = pd.to_datetime(menor_date.Date, dayfirst = True)
+#     base_retornos['Date'] = menor_date.Date
+#     base_prices['Date'] = menor_date.Date
     
-    for ric in rics:         
-        data = pd.read_csv(direccion + ric).dropna(axis = 0)
-        data['Date'] = pd.to_datetime(data.Date, dayfirst = True)
-        times1 = list(data.Date.values)
-        times2 = list(data.Date.values)
-        common_times = list(set(times1) & set(times2))
-        data = data[data.Date.isin(common_times)]
+#     for ric in rics:         
+#         data = pd.read_csv(direccion + ric).dropna(axis = 0)
+#         data['Date'] = pd.to_datetime(data.Date, dayfirst = True)
+#         times1 = list(data.Date.values)
+#         times2 = list(data.Date.values)
+#         common_times = list(set(times1) & set(times2))
+#         data = data[data.Date.isin(common_times)]
+#         data = data.reset_index()
+#         base_retornos[ric.replace('.csv', '') + '_return'] = data.Close / data.Close.shift(1) - 1
+#         base_prices[ric.replace('.csv', '') + '_price'] = data.Close   
+        
+#     return base_prices.dropna(axis = 0), base_retornos.dropna(axis = 0)
+
+# def largo(acciones):
+#     menor = 100000
+#     for i in acciones:
+#         data = pd.read_csv(direccion + i, usecols = ['Close']).dropna()
+#         if data.Close.shape[0] < menor:
+#             menor = data.Close.shape[0]
+#             asset = i
+#     asset_menor = pd.read_csv(direccion + asset, usecols = ['Close','Date']).dropna()
+#     asset_menor.Date = pd.to_datetime(asset_menor.Date, dayfirst = True)
+#     return set(asset_menor.Date)
+
+
+def merge(acciones):
+    data = pd.read_csv(direccion + acciones[1], usecols = ['Date'])
+    data.Date = pd.to_datetime(data.Date, dayfirst = True)
+    time = set(data.Date)
+    for i in acciones:
+        print(time)
+        data = pd.read_csv(direccion + i, usecols = ['Date'])
+        data.Date = pd.to_datetime(data.Date, dayfirst = True)
+        time = set(data.Date) & time
+    return time
+
+def get_data(acciones):
+    df = pd.DataFrame()
+    time = merge(acciones)
+    for i in acciones:
+        data = pd.read_csv(direccion + i, usecols = ['Date', 'Close'])        
+        data.Date = pd.to_datetime(data.Date, dayfirst = True)
+        data = data[data.Date.isin(time)]
         data = data.reset_index()
-        base_retornos[ric.replace('.csv', '') + '_return'] = data.Close / data.Close.shift(1) - 1
-        base_prices[ric.replace('.csv', '') + '_price'] = data.Close   
+        data['returns'] = data.Close/data.Close.shift() - 1
+        df[i.replace('.csv','')] = data.returns
+    return df
+    
+
+
+# def menor(acciones):
+#     """
+#     Inputs:
+#         Acciones: iterable
+#     """
+#     for i in acciones:
         
-    return base_prices.dropna(axis = 0), base_retornos.dropna(axis = 0)
-
-
+            
+#     data = pd.read_csv(direccion + sale, usecols = ['Date', 'Close']).dropna()
+#     data.Date = pd.to_datetime(data.Date, dayfirst = True)
+#     return data.Date
 # df_p['Date'] = pd.to_datetime(df_p.Date, dayfirst = True)
 # df_2p['Date'] = pd.to_datetime(df_2p.Date, dayfirst = True)
 # times1 = list(df_p.Date.values)
@@ -113,16 +147,19 @@ def base_retornos(rics):
 # timess = list (set(times1) & set(times2))
 # df_2p[df_2p.Date.isin(timess)]
 
-
-
 #%%
 # primero vere mis raw materials
-os.chdir('E:/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py/acciones/')
+os.chdir('/media/cristiandavid/CURSOS FEC/UNSA 2022 CC/semestre par/ECONOMIA/tesis/Optimum_portfolio_py/acciones/')
 acciones = os.listdir()
 
-# Creando la base de datos, tambien conocieda como la matriz de precios y retornos
-price_matrix, returns_matrix = base_retornos(acciones)
 
+
+
+# Creando la base de datos, tambien conocieda como la matriz de precios y retornos
+# price_matrix, returns_matrix = base_retornos(acciones)
+df = get_data(acciones)
+df.isnull().sum()
+plt.scatter(df['^STOXX'],df['SAN.MC'])
 # Ahora veamos el enfoque del modelo CAPM
 # Para lo cual necesitamos hacer una regresion lineal teniendo como variable 
 # dependiente al vector de retornos de una accion y como independiente al vector 
@@ -138,41 +175,7 @@ x = np.array(returns_matrix[market_assets[2]])
 # definidos mis activos de mercado, procedo a elegir un activo de este mercado 
 y = np.array(returns_matrix['^VIX_return'])
 
-# Instancio la clase linregress del modulo np.stats
-capm = scipy.stats.linregress(x,y)
-dir(capm)
-capm.slope
-capm.intercept
-
-
-plt.figure()
-plt.scatter(x,y)
-plt.show()
-
-plt.hist(y, bins = 100)
-plt.show()
-# (1 + np.mean(x))**360 - 1 
-# /
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Instancio la clase linregress del modulo np.stat
 
 
 
